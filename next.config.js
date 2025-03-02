ad41195f8e5a5919457b2d5474888d3fdb2dfd3a
angansarker <your.email@example.com>
2025-03-02 01:23:01 -0500

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: [],
   },
+  basePath: '/angan-portfolio',
+  assetPrefix: '/angan-portfolio/',
 }
 
 module.exports = nextConfig 
